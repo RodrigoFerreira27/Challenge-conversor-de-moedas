761ae38e55e79ac87522bd1044fadb51f3302e65
Rodrigo Ferreira <rodrigofsferreira@gmail.com>
2024-10-23 12:43:36 -0300

    atualizacao3

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,7 @@ public class Main {
             opcao = scanner.nextInt();
 
             if (opcao == 1) {
+
                 double valorMenu = 0;
 
                 Scanner digitaMoeda = new Scanner(System.in);
@@ -32,66 +33,68 @@ public class Main {
                 System.out.println("4 Real para Euro");
                 System.out.println("5 Real para Libra");
                 System.out.println("6 Real para Yuan");
-                moedaBase = digitaMoeda.nextInt();
 
-                Scanner digitaValor = new Scanner(System.in);
-                System.out.println("Digite o valor a ser convertido:");
-                valorMenu = digitaValor.nextDouble();
-
-                BuscaCotacao buscaCotacao = new BuscaCotacao();
-                Cotacao cotacaoAtual = buscaCotacao.buscaValor(valorMenu);
-
-                switch (moedaBase) {
-                    case 1: // Real para Dólar
-                        valorConvertido = valorMenu / cotacaoAtual.conversion_rates().BRL() * cotacaoAtual.conversion_rates().USD();
-                        escritaMoedaBase = "reais";
-                        escritaMoedaConvertida = "dólares";
-                        break;
-                    case 2: // Dólar para Real
-                        valorConvertido = valorMenu / cotacaoAtual.conversion_rates().USD() * cotacaoAtual.conversion_rates().BRL();
-                        escritaMoedaBase = "dólares";
-                        escritaMoedaConvertida = "reais";
-                        break;
-                    case 3: // Real para Peso Argentino
-                        valorConvertido = valorMenu / cotacaoAtual.conversion_rates().BRL() * cotacaoAtual.conversion_rates().ARS();
-                        escritaMoedaBase = "reais";
-                        escritaMoedaConvertida = "pesos argentinos";
-                        break;
-                    case 4: // Real para Euro
-                        valorConvertido = valorMenu / cotacaoAtual.conversion_rates().BRL() * cotacaoAtual.conversion_rates().EUR();
-                        escritaMoedaBase = "reais";
-                        escritaMoedaConvertida = "euros";
-                        break;
-                    case 5: // Real para Libra
-                        valorConvertido = valorMenu / cotacaoAtual.conversion_rates().BRL() * cotacaoAtual.conversion_rates().GBP();
-                        escritaMoedaBase = "reais";
-                        escritaMoedaConvertida = "libras";
-                        break;
-                    case 6: // Real para Yuan
-                        valorConvertido = valorMenu / cotacaoAtual.conversion_rates().BRL() * cotacaoAtual.conversion_rates().CNY();
-                        escritaMoedaBase = "reais";
-                        escritaMoedaConvertida = "yuans";
-                        break;
-                    default:
-                        System.out.println("Opção inválida.");
-                        return;
+                while (true) {
+                    moedaBase = scanner.nextInt();
+                    if (moedaBase >= 1 && moedaBase <= 6) {
+                        break; // Saia do loop se a entrada for válida
+                    } else {
+                        System.out.println("Opção inválida, tente novamente:");
+                    }
                 }
+                    Scanner digitaValor = new Scanner(System.in);
+                    System.out.println("Digite o valor a ser convertido:");
+                    valorMenu = digitaValor.nextDouble();
 
+                    BuscaCotacao buscaCotacao = new BuscaCotacao();
+                    Cotacao cotacaoAtual = buscaCotacao.buscaValor(valorMenu);
 
-                //GeradorDeArquivo gerador = new GeradorDeArquivo();
-                //gerador.salvaArquivojson(novoEndereco);
+                    switch (moedaBase) {
+                        case 1: // Real para Dólar
+                            valorConvertido = valorMenu / cotacaoAtual.conversion_rates().BRL() * cotacaoAtual.conversion_rates().USD();
+                            escritaMoedaBase = "reais";
+                            escritaMoedaConvertida = "dólares";
+                            break;
+                        case 2: // Dólar para Real
+                            valorConvertido = valorMenu / cotacaoAtual.conversion_rates().USD() * cotacaoAtual.conversion_rates().BRL();
+                            escritaMoedaBase = "dólares";
+                            escritaMoedaConvertida = "reais";
+                            break;
+                        case 3: // Real para Peso Argentino
+                            valorConvertido = valorMenu / cotacaoAtual.conversion_rates().BRL() * cotacaoAtual.conversion_rates().ARS();
+                            escritaMoedaBase = "reais";
+                            escritaMoedaConvertida = "pesos argentinos";
+                            break;
+                        case 4: // Real para Euro
+                            valorConvertido = valorMenu / cotacaoAtual.conversion_rates().BRL() * cotacaoAtual.conversion_rates().EUR();
+                            escritaMoedaBase = "reais";
+                            escritaMoedaConvertida = "euros";
+                            break;
+                        case 5: // Real para Libra
+                            valorConvertido = valorMenu / cotacaoAtual.conversion_rates().BRL() * cotacaoAtual.conversion_rates().GBP();
+                            escritaMoedaBase = "reais";
+                            escritaMoedaConvertida = "libras";
+                            break;
+                        case 6: // Real para Yuan
+                            valorConvertido = valorMenu / cotacaoAtual.conversion_rates().BRL() * cotacaoAtual.conversion_rates().CNY();
+                            escritaMoedaBase = "reais";
+                            escritaMoedaConvertida = "yuans";
+                            break;
+                        default:
+                            System.out.println("Opção inválida.");
+                            return;
+                }
 
                 System.out.println(+valorMenu + " " + escritaMoedaBase + " " + " são " + valorConvertido + " " + escritaMoedaConvertida + " na cotação atual.");
             } else if (opcao != 2) {
+
                 System.out.println("Opção inválida, tente novamente.");
             }
-
         } while (opcao != 2);
 
         System.out.println("Obrigado por usar o conversor de moedas! Até a próxima!");
         scanner.close();
     }
-
 }
 
 
